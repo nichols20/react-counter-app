e2c1b98d9c41ccc580e47809e4254c1bb43e9caf
nichols20 <johnnytes10@gmail.com>
2021-06-03 00:59:32 -0400

    made tags array, add function to iterate li to obj

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -3,20 +3,26 @@ import React, { Component } from 'react';
 class Counter extends Component {
     state = {
         count: 0,
+        tags: ['tag1','tag2','tag3']
     };
 
     styles = {
         fontSize: 20,
         fontWeight: 'bold'
     };
-
-    render() { 
+    
+    render() {
+        /* in the ul jsx element .map maps each item in 
+        the array to an individual li containing the iterated object */ 
         return (
         <React.Fragment>
           <span style = {this.styles} className= {this.counterClassName()}>
             {this.formatCount()}
           </span>
           <button className= 'btn btn-secondary btn-sm'>Increment</button>
+          <ul>
+              {this.state.tags.map(tag => <li>{tag}</li>)}
+          </ul>
         </React.Fragment>
         );
     }
